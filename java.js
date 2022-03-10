2656dc9597d35865b2351328fb98736bdcdedaa0
unknown <fxphunguyen@gmail.com>
2022-03-10 15:00:05 +0700

    toan tu 3 ngoi

---
+++ java.js
@@ -0,0 +1,15 @@
+
+    //alert("Đã tạo trang web");
+    //var b = prompt("Nhập vào giá trị A");
+    //console.log("Giá trị biến b là:",b);
+    //console.log("Thông báo gì đó");
+    //console.log("Giá trị là :",1)
+    var x, y, z;
+    //x=5;
+    //y=6;
+    x = prompt("Nhập vào giá trị x");
+    y = prompt("Nhâp vào giá trị y");
+    z= parseInt(x) + parseInt(y);
+    alert("Giá trị của z là:" + z);
+    console.log("Giá trị của z là:" + z);
+    
